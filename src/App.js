80c52a590fe2efbb2e0570f5063894b00e85cae0
Vic <victor.ekpo@gmail.com>
2023-04-23 17:19:42 -0500

    feat: scroll to bottom

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 
 const baseURL =
   //  "https://raw.githubusercontent.com/json-iterator/test-data/master";
-  "https://dl.dropboxusercontent.com/s/gxbsj271j5pevec";
-//"https://raw.githubusercontent.com/ozlerhakan/mongodb-json-files/master/datasets";
+  // "https://dl.dropboxusercontent.com/s/gxbsj271j5pevec";
+  "https://raw.githubusercontent.com/ozlerhakan/mongodb-json-files/master/datasets";
 
 const URLPath =
   // "large-file.json"
-  "trades.json"; // has 1 million rows (243MB JSON)
-//"city_inspections.json";
+  //  "trades.json"; // has 1 million rows (243MB JSON)
+  "city_inspections.json";
 
 const testPayLoadURL = `${baseURL}/${URLPath}`;
 
@@ -29,7 +29,7 @@ const fetchNdjson = (setVal) => {
     const exampleReader = ndjsonStream(response.body).getReader();
 
     let result;
-   
+
     while (
       (!result || !result.done) &&
       loadInitial &&
@@ -37,7 +37,7 @@ const fetchNdjson = (setVal) => {
     ) {
       result = await exampleReader.read();
       streamedValues.push(result.value);
-    
+
       if (streamedValues.length === INITIAL_LOAD) {
         setVal((prev) => {
           return [...prev, streamedValues];
@@ -64,6 +64,7 @@ const fetchNdjson = (setVal) => {
       if (!result.done)
         setTimeout(async () => {
           await loadMore();
+          window.scrollTo(0, document.body.scrollHeight);
           console.log("another batch");
         }, TIME_INTERVAL_BETWEEN_LOADS);
     };
@@ -82,7 +83,7 @@ export default () => {
   return (
     <>
       <h2>Streaming Results:</h2>
-      {JSON.stringify(val)}    
+      {JSON.stringify(val)}
     </>
   );
 };
